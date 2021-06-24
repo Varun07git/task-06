e1b702b6b441980047c430fefba9a1c772f343d3
Varun07git <68010309+Varun07git@users.noreply.github.com>
2021-06-25 00:00:58 +0530

    Add files via upload

---
+++ sendmail.py
@@ -0,0 +1,6 @@
+def mail(to, text):
+    import smtplib  
+    server = smtplib.SMTP_SSL( "smtp.gmail.com", 465 )
+    server.login( "varunkushwah15@gmail.com", "Varun {07@}1" )
+    server.sendmail( "varunkushwah15@gmail.com", to, text )
+    server.quit()
