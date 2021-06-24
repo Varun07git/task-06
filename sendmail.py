db8dc9457a434498d61593051686c3e0b6135c57
Varun07git <68010309+Varun07git@users.noreply.github.com>
2021-06-25 00:24:34 +0530

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -1,6 +1,6 @@
 def mail(to, text):
     import smtplib  
     server = smtplib.SMTP_SSL( "smtp.gmail.com", 465 )
-    server.login( "varunkushwah15@gmail.com", "Varun {07@}1" )
+    server.login( "varunkushwah15@gmail.com", "XXXXXXXXXX" )
     server.sendmail( "varunkushwah15@gmail.com", to, text )
     server.quit()
